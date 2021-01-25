242c6ddb28a83e53b4179dedaaec98221fc3e4b5
rococoscout <rococoscout@gmail.com>
2021-01-25 07:52:16 -0800

    update API

--- JS/addbutton.js
+++ JS/addbutton.js
@@ -1,11 +1,36 @@
 
+
+function ltor(list){
+  list  = JSON.parse(list);
+  console.log(list);
+  message = '<div class="row header-row"><div class="col-sm">description</div><div class="col-sm">rule</div><div class="col-sm">ruleID</div><div class="col-sm">title</div></div>';
+  for(i in list){
+    message += '<div class="row">';
+    console.log(i);
+    for(ii in list[i]){
+
+      message += '<div class="col-sm">';
+      message += list[i][ii];
+      message+= '</div>';
+
+    }
+
+    message+= '</div>';
+  }
+
+  return message;
+}
+
+
+
+
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
-   document.getElementById("demo").innerHTML = this.responseText;
+   document.getElementById("demo").innerHTML = ltor(this.responseText);
   }
 };
-xhttp.open("POST", "PHP/admin.php", true);
+xhttp.open("GET", "http://10.1.83.57:5000/api/entries/rules", true);
 xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 xhttp.send();
 
@@ -15,15 +40,23 @@ function addrule(){
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-     document.getElementById("demo").innerHTML = this.responseText;
+     console.log(this.responseText);
+     var xhttp1 = new XMLHttpRequest();
+     xhttp1.onreadystatechange = function() {
+       if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("demo").innerHTML = ltor(this.responseText);
+       }
+     };
+     xhttp1.open("GET", "http://10.1.83.57:5000/api/entries/rules", true);
+     xhttp1.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+     xhttp1.send();
     }
   };
-  xhttp.open("POST", "PHP/admin.php", true);
+  xhttp.open("POST", "http://10.1.83.57:5000/api/entries/addRule", true);
   xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
   xhttp.send("rule="+document.getElementById('rule').value+"&title="+document.getElementById('title').value+"&description="+document.getElementById('description').value);
   $('#exampleModalCenter').modal('hide')
   document.getElementById('title').value = "";
   document.getElementById('description').value="";
   document.getElementById('rule').value ="";
-  console.log("bye");
 }
--- JS/input.js
+++ JS/input.js
@@ -1,31 +1,62 @@
+function resolveAfter1Seconds(){
+  return new Promise(resolve => {
+    setTimeout(()=>{
+      resolve('resolved');
+    },1000);
+  });
+}
+
+async function thinking(response){
+  console.log('calling');
+  document.getElementById('text').innerHTML +='<div class="yours messages thinking"><div class="message thinking"><div id = "child1"></div><div id = "child2"></div><div id = "child3"></div>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</div></div>'
+  const result = await resolveAfter1Seconds();
+  $('.thinking').remove();
+  document.getElementById('text').innerHTML += '<div class="yours messages"><div class="message last">'+ response+'</div></div>';
+  console.log('result');
+
+}
+
+
+
 document.getElementById('input').addEventListener("keyup", function(event) {
 
   if (event.keyCode === 13) {
-    console.log("why")
     var text = document.getElementById('input').value;
+    if (text == "" || /^\s*$/.test(text))
+      return;
     var message = "";
     var current = document.getElementsByClassName('current');
     console.log(current.length)
-    if(current.length!=1){
-    message = '<div class="mine messages current">';
-    message += '<div class="message last">';
-    message += text;
-    message += '</div></div>';
-    document.getElementById('text').innerHTML += message;
-    }
-    else{
-      var last = current[0].getElementsByClassName('last')[0]
-      last.classList.remove("last");
-      console.log(last)
-      message = '<div class="message last">';
+      if(current.length!=1){
+      message = '<div class="mine messages current">';
+      message += '<div class="message last">';
       message += text;
-      message += '</div>';
-      current[0].innerHTML += message;
+      message += '</div></div>';
+      document.getElementById('text').innerHTML += message;
+      }
+      else{
+        var last = current[0].getElementsByClassName('last')[0]
+        last.classList.remove("last");
+        message = '<div class="message last">';
+        message += text;
+        message += '</div>';
+        current[0].innerHTML += message;
 
-    }
+      }
     document.getElementById('input').value = "";
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+       thinking(this.response);
+
+       current[0].classList.remove("current");
+
+
+      }
+    };
+    xhttp.open("POST", "http://10.1.83.57:5000/api/input", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("input="+text);
   }
 
-
-
-});
+  });
