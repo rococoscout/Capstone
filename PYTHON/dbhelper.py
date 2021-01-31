c28a053d3cf53b72b4ca33022171ff74fe3e37fc
Greg Polmatier <m215394@usna.edu>
2021-01-31 16:02:21 -0500

    Created Rule class and alter helper

--- PYTHON/dbhelper.py
+++ PYTHON/dbhelper.py
@@ -21,6 +21,15 @@ class DBHelper:
         
         self.cursor = self.con.cursor()
 
+    def noDictConnect(self):
+        self.con = pymysql.connect(host=self.host,
+                             user = self.user,
+                             passwd = self.password,
+                             db = self.db,
+                             cursorclass=pymysql.cursors.Cursor)
+        
+        self.cursor = self.con.cursor()
+
     def __disconnect__(self):
         self.con.close()
 
@@ -30,6 +39,14 @@ class DBHelper:
         result = self.cursor.fetchall()
         self.__disconnect__()
         return result
+
+    def fetchNoDict(self, sql):
+        self.noDictConnect()
+        self.cursor.execute(sql)
+        result = [x[0] for x in self.cursor.fetchall()]
+        self.__disconnect__()
+        return result
+
 
     def execute(self, sql):
         self.__connect__()
---
+++ PYTHON/rule.py
@@ -0,0 +1,78 @@
+# A class to store a Rule, based on the capstone Schema 
+# MIDN 1/C Polmatier
+from dbhelper import DBHelper
+import json
+
+class Rule:
+    db = DBHelper()
+    def __init__(self, ID, regex, answers, questions, vector):
+        self.id = ID                # rule ID 
+        self.regex = regex          # Regular Expression
+        self.answers = answers      # List of answers based on the Rule
+        self.questions = questions  # List of questions that have been asked that asssociate with the rule
+        self.vector = vector        # The total vector representation of the list of Questions
+
+
+    '''
+    update total vector of this Rule in the database
+    '''
+    def updateVector(self):
+        json_v = json.dumps(self.vector)
+        print(json_v)
+        sql = f"UPDATE Rules SET totalVector='{json_v}' WHERE idRules={self.id};"
+        return Rule.db.execute(sql)
+
+
+    '''
+    Updates regex and total vector into the database schema 
+    '''
+    def updateRule(self):
+        json_v = json.dumps(self.vector)
+        sql = f"UPDATE Rules SET regex='{self.regex}' totalVector='{json_v}' WHERE idRules={self.id};"
+        return Rule.db.execute(sql)
+        return None
+
+    def addQuestion(self, q):
+        sql = f"INSERT INTO Questions (question, idRules) VALUES ('{q}', {self.id});"
+        return Rule.db.execute(sql)
+
+
+    '''
+    Function returns all the rules in the database including regex(if exists), 
+    list of answers, list of questions, and the total vector
+
+    Returns a list of class Rules
+    '''
+    @staticmethod
+    def getRules():
+        rules = []
+
+        # get rules from sql
+        sql = "SELECT idRules, totalVector, regex FROM Rules;"
+        ruleEntries = Rule.db.fetch(sql)
+
+        #print(ruleEntries)
+        # for all rules
+        for r in ruleEntries:
+            # get id, totalVector, and regex 
+            ID = r['idRules']
+            vec = json.loads(r['totalVector'])
+            reg = r['regex']
+
+            # based off id get list of questions
+            sql = f"SELECT question FROM Questions WHERE Questions.idRules = {ID};"
+            qs = Rule.db.fetchNoDict(sql)
+
+            # based off id get list of answers
+            sql = f"SELECT answer FROM Answers WHERE Answers.idRules = {ID};"
+            ans = Rule.db.fetchNoDict(sql)
+
+            # create Rule object and append
+            rules.append(Rule(ID, reg, ans, qs, vec))
+        
+        # return list
+        return rules
+
+if __name__ == "__main__":
+   
+    
