885362dcb7f8ad1984ab3ed3c778ab34dea40630
Greg Polmatier <m215394@usna.edu>
2021-03-15 12:55:02 -0400

    all

--- PYTHON/api.py
+++ PYTHON/api.py
@@ -45,8 +45,9 @@ def api_input():
 
     ans = getAnswer(inp)
     generic = "I'm not too sure about"
+    language = "It seems that you have entered input that is not in English"
     print(generic in ans)
-    if generic in ans:
+    if generic in ans or generic in language:
         return jsonify([{'idAnswers':-1, 'answer': ans}])
 
     sql = f'SELECT idAnswers, answer FROM Answers WHERE answer="{ans}";'
